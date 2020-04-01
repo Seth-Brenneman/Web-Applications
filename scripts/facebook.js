42fb8f6578c7b5ef0c20f37644508d9776a3bebe
Seth-Brenneman <seth.brenneman@emu.edu>
2020-04-01 14:53:10 -0400

    Finished commentWithEnter() function

--- scripts/facebook.js
+++ scripts/facebook.js
@@ -1,5 +1,10 @@
 var main = function () { 
     "use strict"; 
+    commentWithClick();
+    commentWithEnter();
+};
+
+var commentWithClick = function () {
     $(".comment-input button").on("click", function (event) { 
         var $new_comment=$(".comment-input input").val();
         if ($new_comment == "") {
@@ -9,8 +14,21 @@ var main = function () {
             $(".comment-input input").val("");
         }
     });
+}
+
+var commentWithEnter = function () {
     $(".comment-input input").on("keypress", function (event) { 
-        console.log("hello world!"); 
+        var code = event.keyCode;
+        var $new_comment=$(".comment-input input").val();
+        if ($new_comment == "" && code == 13) {
+            window.alert("You cannot post an empty comment.");
+        } else {
+            if (code == 13) {
+                $(".comments").append($("<p>").text($new_comment));
+                $(".comment-input input").val("");
+            }
+            
+        }
     }); 
-}; 
+}
 $(document).ready(main);
