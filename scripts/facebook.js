379892eb1b761392c810492f0a0e0c57849ceb07
Seth-Brenneman <seth.brenneman@emu.edu>
2020-04-01 12:28:20 -0400

    Added new keypress event but it still needs work

--- scripts/facebook.js
+++ scripts/facebook.js
@@ -2,8 +2,15 @@ var main = function () {
     "use strict"; 
     $(".comment-input button").on("click", function (event) { 
         var $new_comment=$(".comment-input input").val();
-        
-        $(".comments").append($new_comment.text());
+        if ($new_comment == "") {
+            window.alert("You cannot post an empty comment.");
+        } else {
+            $(".comments").append($("<p>").text($new_comment));
+            $(".comment-input input").val("");
+        }
+    });
+    $(".comment-input input").on("keypress", function (event) { 
+        console.log("hello world!"); 
     }); 
 }; 
 $(document).ready(main);
