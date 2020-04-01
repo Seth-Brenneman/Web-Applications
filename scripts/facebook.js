913026e0778d0ee7285f14fd36793fbee650292c
Seth-Brenneman <seth.brenneman@emu.edu>
2020-04-01 10:03:49 -0400

    Changed main function

--- scripts/facebook.js
+++ scripts/facebook.js
@@ -1,11 +1,9 @@
 var main = function () { 
     "use strict"; 
     $(".comment-input button").on("click", function (event) { 
-        var $new_comment=$("<p>");
-
-        $new_comment.text("this is another new comment");
-        $(".comments").append($new_comment);
-        //$(".comments").append("<p>this is a new comment</p>"); 
+        var $new_comment=$(".comment-input input").val();
+        
+        $(".comments").append($new_comment.text());
     }); 
 }; 
 $(document).ready(main);
