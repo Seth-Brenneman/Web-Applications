ed0ce46ae3e0c012ff9d303670a0f5d2f36a7cb5
Seth-Brenneman <seth.brenneman@emu.edu>
2020-04-03 14:26:27 -0400

    Added clear and remove all comments buttons

--- scripts/facebook.js
+++ scripts/facebook.js
@@ -1,20 +1,28 @@
 var main = function () { 
     "use strict"; 
     //inputs somethign if the '+' button is clicked
-    $(".comment-input button").on("click", function (event) { 
-        addCommentFormInputBox();
+    $(".comment-input #submit").on("click", function (event) { 
+        addCommentFromInputBox();
+    });
+
+    $(".comment-input #clear").on("click", function (event) { 
+        clearCommentFromInputBox();
     });
     
     //inputs something if the 'enter' key is pressed
     $(".comment-input input").on("keypress", function (event) { 
         var code = event.keyCode;
         if (code == 13) { //checks is the enter key is pressed, then executes the input function
-            addCommentFormInputBox();
+            addCommentFromInputBox();
         }
-    });     
+    });
+    
+    $(".remove-comments #remove").on("click", function (event) { 
+        clearAllComments();
+    });
 };
 //algorithm for posting new comments
-var addCommentFormInputBox = function() {
+var addCommentFromInputBox = function() {
     var $new_comment=$(".comment-input input").val();
 
     if ($new_comment == "") {
@@ -23,5 +31,13 @@ var addCommentFormInputBox = function() {
         $(".comments").append($("<p>").text($new_comment).fadeIn());
         $(".comment-input input").val("");
      }
+}
+
+var clearCommentFromInputBox = function() {
+    $(".comment-input input").val("");
+}
+
+var clearAllComments = function() {
+    $(".comments").empty();
 }
 $(document).ready(main);
