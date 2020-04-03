d7ff0ae355a8c91e13f0dd9bcf710425e7ff9b76
Seth-Brenneman <seth.brenneman@emu.edu>
2020-04-03 13:30:49 -0400

    Added a single comment posting algorithm that can be used for both events

--- scripts/facebook.js
+++ scripts/facebook.js
@@ -1,33 +1,27 @@
 var main = function () { 
     "use strict"; 
-    commentWithClick();
-    commentWithEnter();
-};
-
-var commentWithClick = function () {
+    //inputs somethign if the '+' button is clicked
     $(".comment-input button").on("click", function (event) { 
-        var $new_comment=$(".comment-input input").val();
-        if ($new_comment == "") {
-            window.alert("You cannot post an empty comment.");
-        } else {
-            $(".comments").append($("<p>").text($new_comment).fadeIn());
-            $(".comment-input input").val("");
-        }
+        addCommentFormInputBox();
     });
-}
-
-var commentWithEnter = function () {
+    
+    //inputs something if the 'enter' key is pressed
     $(".comment-input input").on("keypress", function (event) { 
         var code = event.keyCode;
-        var $new_comment=$(".comment-input input").val();
-        if ($new_comment == "" && code == 13) {
-            window.alert("You cannot post an empty comment.");
-        } else {
-            if (code == 13) {
-                $(".comments").append($("<p>").text($new_comment).fadeIn());
-                $(".comment-input input").val("");
-            }
+        if (code == 13) { //checks is the enter key is pressed, then executes the input function
+            addCommentFormInputBox();
         }
-    }); 
+    });     
+};
+//algorithm for posting new comments
+var addCommentFormInputBox = function() {
+    var $new_comment=$(".comment-input input").val();
+
+    if ($new_comment == "") {
+        window.alert("You cannot post an empty comment.");
+     } else {
+        $(".comments").append($("<p>").text($new_comment).fadeIn());
+        $(".comment-input input").val("");
+     }
 }
 $(document).ready(main);
