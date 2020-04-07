b258603f20d247978e7a2e0dd7f72c0229149497
Seth-Brenneman <seth.brenneman@emu.edu>
2020-04-07 18:23:16 -0400

    Working on a primality function in app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,32 @@
 var main = function () { 
-    "use strict"; 
-     window.alert("hello world!");
+    var main = function () { 
+        "use strict"; 
+        // create and hide our content as a div
+        var $content = $("<div>Hello World!</div>").hide();
+        var $moreContent = $("<div>Goodbye World!</div>").hide(); 
+        
+        // append the content to the body element
+        $("body").append($content);
+        
+        $content.slideDown(2000, function(){
+            // append the second content to the body 
+            $("body").append($moreContent);
+            
+            // fade in the second content
+            $moreContent.fadeIn();
+    
+        });
+    
+        var primality = function(num) {
+            if (num == 2 || 3 || 5 || 7 || 11) {
+                return "It is prime"
+            } else if (num == 0 || 1) {
+                return "It is not prime"
+            }
+        }
+     };
+     $(document).ready(main);
+     
  };
  $(document).ready(main);
  
